8502d8f1fa47bf183ec8ecbaf3de6ac4d0c572bd
Azure Pipelines <SpeedTechSolutions@visualstudio.com>
2020-07-03 12:18:49 +0000

    Release Version 2020.07.03.406

---
+++ precache-manifest.1c6d0a2e3af0d91fee4a66a57b474d5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c0b68949ad8f20fb670a267aacb532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c50a0b69ba58951c85e",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "ae57ccbed9e94990f9de",
+    "url": "/static/js/2.a080cf4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a080cf4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c50a0b69ba58951c85e",
+    "url": "/static/js/main.c936960c.chunk.js"
+  },
+  {
+    "revision": "4b7e1f98160ce7c45539",
+    "url": "/static/js/runtime-main.d10ac219.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
