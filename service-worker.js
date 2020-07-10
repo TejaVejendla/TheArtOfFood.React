6df0cd626c0fed33991fcead1a753a22c1ceb8eb
Azure Pipelines <SpeedTechSolutions@visualstudio.com>
2020-07-10 12:11:38 +0000

    Release Version 2020.07.10.415

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TheArtOfFood.React/precache-manifest.7284fb439a34576ccd994ccd566b1074.js"
+  "/TheArtOfFood.React/precache-manifest.01732d69ece77abf2783cc9e8245d027.js"
 );
 
 self.addEventListener('message', (event) => {
